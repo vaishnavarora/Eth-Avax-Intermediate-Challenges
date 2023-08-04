abbfa46fd7f31f28f95a40851eab1b42a9486937
Vaishnav Arora <vaishgabha@gmail.com>
2023-08-05 00:13:39 +0530

    Update SmartOwner.sol

--- SmartOwner.sol
+++ SmartOwner.sol
@@ -1,1 +1,37 @@
+/*
+Write a smart contract that allows only the owner to access and call all the functions of the contract.
+Hint: use modifiers to restrict access
+*/
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+contract OwnerOnly {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeOwner(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid owner address");
+        owner = newOwner;
+    }
+
+    function someFunction() public onlyOwner {
+        // Function logic here
+    }
+
+    function anotherFunction() public onlyOwner {
+        // Function logic here
+    }
+
+    // We can add more functions as needed, all restricted to the owner only
+}
