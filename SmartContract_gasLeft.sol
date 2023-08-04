abe101e3ae0625374d0c79c4786e2d02066414fd
Vaishnav Arora <vaishgabha@gmail.com>
2023-08-05 00:59:17 +0530

    Update SmartContract_gasLeft.sol

--- SmartContract_gasLeft.sol
+++ SmartContract_gasLeft.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract globalFunc_In_MsgContext {
+    uint256 public gasLeft;
+    address public senderAddress;
+    uint256 public value;
+
+    constructor() payable {
+        gasLeft =  gasleft();
+        senderAddress = msg.sender;
+        value = msg.value;
+    }
+}
