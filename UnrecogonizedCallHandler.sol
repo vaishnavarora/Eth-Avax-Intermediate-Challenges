dbbefd714cc2cf8aa9ad811964e98a43c16711ba
Vaishnav Arora <vaishgabha@gmail.com>
2023-08-05 10:55:01 +0530

    Update UnrecogonizedCallHandler.sol

--- UnrecogonizedCallHandler.sol
+++ UnrecogonizedCallHandler.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+contract EtherReceiver {
+    address payable owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    fallback() external payable {
+        require(msg.value > 0, "No ether sent");
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only the owner can withdraw");
+        owner.transfer(address(this).balance);
+    }
+
+    recieve() external payable {
+        revert("Unrecognized call");
+    }
+}
