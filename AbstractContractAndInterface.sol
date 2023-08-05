5270db6f6ae51bf91aae5bb99c9e0ce9fed0855e
Vaishnav Arora <vaishgabha@gmail.com>
2023-08-05 14:20:53 +0530

    Update AbstractContractAndInterface.sol

--- AbstractContractAndInterface.sol
+++ AbstractContractAndInterface.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+interface CalculatorInterface {
+    function add(uint256 a, uint256 b) external pure returns (uint256);
+}
+abstract contract BaseCalculator {
+    function subtract(uint256 a, uint256 b) public pure virtual returns (uint256);
+}
+
+contract AdvancedCalculator is CalculatorInterface, BaseCalculator {
+    function add(uint256 a, uint256 b) external pure override returns (uint256) {
+        return a + b;
+    }
+
+    function subtract(uint256 a, uint256 b) public pure override returns (uint256) {
+        return a - b;
+    }
+}
